d838ab3078d67a7a93ab40b499e4bfd2f5d9dfde
Neha Sharma <neha.college2025@gmail.com>
2026-02-01 16:44:12 -0700

    Update Water.java

--- Water.java
+++ Water.java
@@ -8,15 +8,28 @@ import java.awt.*;
 public class Water extends Icons {
     private boolean used = false;
 
+    /**
+     * Constructs a Water trap
+     * @param x The x-coordinate of the water.
+     * @param y The y-coordinate of the water.
+     */
     public Water(int x, int y) {
         super(x, y, "water");
     }
 
+    /**
+     * Determines if the water should be removed from the game.
+     * @return true if the water was hit.
+     */
     @Override
     public boolean shouldRemove() {
         return wasHit;
     }
 
+    /**
+     * Draws the water
+     * @param g The Graphics object used to draw the water.
+     */
     @Override
     public void draw(Graphics g) {
         g.setColor(new Color(100, 149, 237));
@@ -27,11 +40,21 @@ public class Water extends Icons {
         g.fillOval(x + 11, y + 11, 4, 4);
     }
 
+    /**
+     * Checks whether the water is an obstacle.
+     * @return true, as the water is an obstacle.
+     */
     @Override
     public boolean isObstacle() {
         return true;
     }
 
+    /**
+     * Handles the collision between the duck and the water.
+     * @param duck The PlayerDuck object that collided with the water.
+     * @param scoreManager The ScoreManager to update the score.
+     * @param isJumping Whether the duck is jumping.
+     */
     @Override
     public void handleCollision(PlayerDuck duck, ScoreManager scoreManager, boolean isJumping) {
         if (isJumping) {
@@ -47,14 +70,21 @@ public class Water extends Icons {
         }
     }
 
+    /**
+     * Returns the front bounding rectangle for collision detection.
+     * @return A Rectangle representing the front of the water.
+     */
     @Override
     public Rectangle getFrontBounds() {
         return new Rectangle(x + 21, y + 6, 3, 13);
     }
 
+    /**
+     * Returns the back bounding rectangle for collision detection.
+     * @return A Rectangle representing the back of the water.
+     */
     @Override
     public Rectangle getBackBounds() {
         return new Rectangle(x + 1, y + 6, 3, 13);
     }
-
 }
