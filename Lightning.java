ba10d3131de032793d9e81c9987c407f3641bff0
Neha Sharma <neha.college2025@gmail.com>
2026-02-01 16:31:13 -0700

    Update Lightning.java

--- Lightning.java
+++ Lightning.java
@@ -3,9 +3,6 @@
  * when collected, all the turtles disappear for 5 seconds
  * @author: Neha Sharma
  * Collaborators: Kaushiki Kapoor, Bradley Nguyen
- * Teacher Name: Mrs. Bailey
- * Period: 1
- * Due Date: 05-12-25
  */
 import java.awt.*;
 
@@ -118,4 +115,5 @@ public class Lightning extends Icons {
     public Rectangle getBounds() {
         return new Rectangle(x, y, 20, 20);
     }
+
 }
