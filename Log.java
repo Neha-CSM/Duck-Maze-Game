3d03c378f03743197c57699aa135a9d583bbca7c
Neha Sharma <neha.college2025@gmail.com>
2026-02-01 16:38:12 -0700

    Update Log.java

--- Log.java
+++ Log.java
@@ -8,15 +8,28 @@ import java.awt.*;
 public class Log extends Icons {
     private boolean used = false;
 
+    /**
+     * Constructs a Log
+     * @param x The x-coordinate of the log.
+     * @param y The y-coordinate of the log.
+     */
     public Log(int x, int y) {
         super(x, y, "log");
     }
 
+    /**
+     * Determines if the log should be removed from the game.
+     * @return true if the log was hit.
+     */
     @Override
     public boolean shouldRemove() {
         return wasHit;
     }
 
+    /**
+     * Draws the log
+     * @param g The Graphics object used to draw the log.
+     */
     @Override
     public void draw(Graphics g) {
         g.setColor(new Color(160, 82, 45));
@@ -28,11 +41,21 @@ public class Log extends Icons {
         g.drawLine(x + 4, y + 13, x + 20, y + 13);
     }
 
+    /**
+     * Checks whether the log is an obstacle.
+     * @return true, as the log is an obstacle.
+     */
     @Override
     public boolean isObstacle() {
         return true;
     }
 
+    /**
+     * Handles the collision between the duck and the log.
+     * @param duck The PlayerDuck object that collided with the log.
+     * @param scoreManager The ScoreManager to update the score.
+     * @param isJumping Whether the duck is jumping.
+     */
     @Override
     public void handleCollision(PlayerDuck duck, ScoreManager scoreManager, boolean isJumping) {
         if (isJumping) {
@@ -48,14 +71,21 @@ public class Log extends Icons {
         }
     }
 
+    /**
+     * Returns the front bounding rectangle for collision detection.
+     * @return A Rectangle representing the front of the log.
+     */
     @Override
     public Rectangle getFrontBounds() {
         return new Rectangle(x + 20, y, 5, 25);
     }
 
+    /**
+     * Returns the back bounding rectangle for collision detection.
+     * @return A Rectangle representing the back of the log.
+     */
     @Override
     public Rectangle getBackBounds() {
         return new Rectangle(x, y, 5, 25);
     }
-
 }
