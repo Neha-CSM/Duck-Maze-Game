ebfc57cb6d6c55ee9455d332aaa6adc6d691dfd5
Neha Sharma <neha.college2025@gmail.com>
2026-02-01 16:46:55 -0700

    Update Turtle.java

--- Turtle.java
+++ Turtle.java
@@ -10,19 +10,36 @@ public class Turtle extends Icons {
     private boolean used = false;
     private Lightning lightningRef;
 
+    /**
+     * Constructs a Turtle
+     * @param x The x-coordinate of the turtle.
+     * @param y The y-coordinate of the turtle.
+     * @param lightningRef Reference to lightning for power-up logic.
+     */
     public Turtle(int x, int y, Lightning lightningRef) {
         super(x, y, "turtle");
         this.lightningRef = lightningRef;
     }
 
     public void applyEffect(PlayerDuck duck) {
+        // No effect for turtle
     }
 
+    /**
+     * Determines if the turtle should be removed from the game.
+     * @return true if it was hit or if lightning is active.
+     */
     @Override
     public boolean shouldRemove() {
         return wasHit || (lightningRef != null && lightningRef.isEffectActive());
     }
 
+    /**
+     * Handles the collision between the duck and the turtle.
+     * @param duck The PlayerDuck object that collided with the turtle.
+     * @param scoreManager The ScoreManager to update the score.
+     * @param isJumping Whether the duck is jumping.
+     */
     @Override
     public void handleCollision(PlayerDuck duck, ScoreManager scoreManager, boolean isJumping) {
         if (isJumping) {
@@ -32,17 +49,25 @@ public class Turtle extends Icons {
             }
         } else {
             if (!wasHit) {
-                wasHit = true; // one-time collision handling
+                wasHit = true; 
                 scoreManager.losePoints(25, duck); 
             }
         }
     }
 
+    /**
+     * Checks whether the turtle is an obstacle.
+     * @return true, as the turtle is an obstacle.
+     */
     @Override
     public boolean isObstacle() {
         return true;
     }
 
+    /**
+     * Draws the turtle
+     * @param g The Graphics object used to draw the turtle.
+     */
     @Override
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
@@ -61,14 +86,21 @@ public class Turtle extends Icons {
         g.drawArc(x + 23, y + 16, 4, 3, 0, 180);
     }
 
+    /**
+     * Returns the front bounding rectangle for collision detection.
+     * @return A Rectangle representing the front of the turtle.
+     */
     @Override
     public Rectangle getFrontBounds() {
         return new Rectangle(x + 20, y, 10, 30);
     }
 
+    /**
+     * Returns the back bounding rectangle for collision detection.
+     * @return A Rectangle representing the back of the turtle.
+     */
     @Override
     public Rectangle getBackBounds() {
         return new Rectangle(x, y, 5, 30);
     }
-
 }
